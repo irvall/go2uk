26133c67fa146f5e475db6c4d5d62f6ff788c205
Johan Irvall Rasmussen <johanirvall@gmail.com>
2021-05-23 17:01:18 +0200

    small refactoring; also made more british

--- go2uk.py
+++ go2uk.py
@@ -2,36 +2,50 @@
 import sys
 import requests
 import lxml.html as lh
-import pandas as pd
 from termcolor import colored
+url = 'https://www.gov.uk/guidance/red-amber-and-green-list-rules-for-entering-england'
 cmap = {}
+
+
 def populate():
-    url='https://www.gov.uk/guidance/red-amber-and-green-list-rules-for-entering-england'
     page = requests.get(url)
     doc = lh.fromstring(page.content)
-    tables = doc.xpath('//table')
-    for table in tables:
-        color = None
-        for t in table:
-            if t.tag == 'thead':
-                color = t.text_content().split()[0].lower()
-                cmap[color] = []
+    for tables in doc.xpath('//table'):
+        colour = None
+        for table in tables:
+            if table.tag == 'thead':
+                colour = table.text_content().split()[0].lower()
+                cmap[colour] = []
             else:
-                countries = [name for name in t.text_content().split('\n') if len(name.strip()) > 1 and not name.strip().startswith('Moved')]
+                countries = [name for name in table.text_content().split('\n')
+                             if len(name.strip()) > 1 and not name.strip().startswith('Moved')]
                 for c in countries:
                     key = c.strip().split()[0].lower()
-                    cmap[key] = color
-                    cmap[color].append(c.strip())
-    
+                    cmap[key] = colour
+                    cmap[colour].append(c.strip())
+
+
+def show_colour(*colours):
+    delimit = '\n* '
+    for colour in colours:
+        cs = cmap[colour]
+        text_colour = "yellow" if colour == "amber" else colour
+        print(
+            f'{colored(colour.capitalize(), text_colour)} ({len(cs)} countries):', end=delimit)
+        print(delimit.join(cmap[colour]))
+        print()
+
+
+populate()
 if len(sys.argv) > 1:
-    populate()
-    name = sys.argv[1].strip().lower()
-    if name in ['red', 'amber', 'green']:
-        color = 'yellow' if name == 'amber' else name
-        for c in cmap[name]:
-            print(colored(c, color))
-    elif name in cmap:
-        color = cmap[name]
-        print(f'{sys.argv[1].capitalize()}: {colored(color, "yellow" if color == "amber" else color)}')
+    arg = sys.argv[1].strip().lower()
+    if arg in ['red', 'amber', 'green']:
+        show_colour(arg)
+    elif arg in cmap:
+        colour = cmap[arg]
+        print(
+            f'{sys.argv[1].capitalize()}: {colored(colour, "yellow" if colour == "amber" else colour)}')
     else:
-        print(f'Couldn\'t find country {colored(name.capitalize(), "blue")}')
+        print(f'Couldn\'t find country {colored(arg.capitalize(), "blue")}')
+else:
+    show_colour('red', 'amber', 'green')
